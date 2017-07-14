310951b3b7de4107a2f8f458c76090044530a471
David May-Miller <David.May-Miller@softwire.com>
2017-07-14 14:47:58 +0100

    Finished Task 1

--- workspace/template.ts
+++ workspace/template.ts
@@ -1,13 +1,56 @@
 import request = require('request');
+import { Bus } from "./bus";
 
 export class Template {
 
     public run(): void {
-        request('https://api.tfl.gov.uk/StopPoint/490008660N/Arrivals?app_id=1aeb84cc&app_key=8319a3e05b400574fdc7d0db5b0d3bfb', function (error, response, body) {
-        console.log('error:', error); // Print the error if one occurred 
-        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received 
-        console.log('body:', body); // Print the HTML for the Google homepage. 
-});
+        const requestPromise = new Promise((resolve, reject) => {
+            request('https://api.tfl.gov.uk/StopPoint/490008660N/Arrivals?app_id=1aeb84cc&app_key=8319a3e05b400574fdc7d0db5b0d3bfb', function (error, response, body) {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(body);
+                }
+            });
+            
+        });
+        requestPromise.then((body)=>this.quickestFive(this.parseJson(body))).catch((err)=> console.log(err));
+    }
+
+    private parseJson(body): Bus[] {
+        const data = JSON.parse(body);
+        let busList: Bus[] = [];
+        //console.log(body);
+        for(let i = 0; i < data.length; i ++)
+        {
+            let busData = data[i];
+            let bus = new Bus(busData["id"], busData["operationType"], busData["vehicleId"], busData["lineId"], busData["lineName"], busData["platformName"], busData["direction"], busData["bearing"], busData["destinationNaptanId"],busData["destinationName"], busData["timeToStation"], busData["expectedArrival"], busData["towards"]);
+            busList[i] = bus;
+        }
+        console.log(busList);
+        return busList;
+    }
+
+    private quickestFive(busList: Bus[]): void {
+        let quickest: Bus[] = [];
+        for (let i = 0; i < busList.length; i++) {
+            if (quickest.length === 0) {
+                quickest[0] = busList[i];
+            } else {
+                for (let j = 0; j < quickest.length; j++){
+                    if (busList[i].getTimeToStation() < quickest[j].getTimeToStation()) {
+                        quickest.splice(j,0,busList[i]);
+                        if (quickest.length > 5) {
+                            quickest.splice(quickest.length-1,1);
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+        for (let i = 0; i< quickest.length; i++) {
+            console.log(quickest[i].getBusInfo());
+        }
     }
 }
 
