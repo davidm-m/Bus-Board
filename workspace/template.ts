7d1d8028ce111853254e2620b045a2c1efadbf6b
David May-Miller <David.May-Miller@softwire.com>
2017-07-14 16:41:34 +0100

    Finished Task 2 and 3

--- workspace/template.ts
+++ workspace/template.ts
@@ -12,12 +12,91 @@ export class Template {
         rl.on('line', (input: string) => {
             if (input.startsWith("Print Buses ")) {
                 this.printNextBuses(input.substring(12));
+            } else if (input.startsWith("Find Near ")) {
+                this.findNear(input.substring(10));
             }
         });
     }
 
+    private findNear(postcode: string): void {
+        const requestPromise = new Promise((resolve, reject) => {
+            request('http://api.postcodes.io/postcodes/'+postcode, function (error, response, body) {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(body);
+                }
+            });
+        })
+        requestPromise.then((body)=>{
+
+            this.getStops(this.returnLongLat(body));
+        });
+    }
+
+    private returnLongLat(body):number[]
+    {
+        let longLat: number[] = [];
+        const data = JSON.parse(body);
+        longLat[0] = data["result"]['longitude'];
+        longLat[1] = data["result"]['latitude'];
+        return longLat;
+
+    }
+
+    private getStops(longLat: number[]):void
+    {
+        const requestPromise = new Promise((resolve, reject) => {
+            request('https://api.tfl.gov.uk/StopPoint?stopTypes=NaptanPublicBusCoachTram&radius=200&useStopPointHierarchy=false&modes=bus&returnLines=false&lat='+longLat[1].toString()+'&lon='+longLat[0].toString()+'&app_id=1aeb84cc&app_key=8319a3e05b400574fdc7d0db5b0d3bfb', function(error, response, body) {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(body);
+                }
+            })
+        });
+        requestPromise.then((body)=>this.getClosest(body));
+    }
+
+    private getClosest(body) {
+        const data = JSON.parse(body);
+        for (let i = 0; i < 2; i++) {
+            if (i >= data["stopPoints"].length) {
+                console.log("Not enough bus stops in area");
+                break;
+            } else {
+                const stop = data["stopPoints"][i];
+                const busPromise = this.getBusRequest(stop["id"]);
+                busPromise.then((body) => {
+                    console.log(stop["commonName"]+" at "+stop["distance"].toString()+" meters away");
+                    this.printBuses(body);
+                })
+                
+                //this.printNextBuses(stop["id"]);
+            }
+        }
+    }
+
+    private printBuses(body):void{
+        //console.log(body);
+        const buses: Bus[] = this.parseJsonBus(body);
+        const quickest: Bus[] = this.quickestFive(buses);
+        for (let i = 0; i< quickest.length; i++) {
+            console.log(quickest[i].getBusInfo());
+        }
+    }
+
     private printNextBuses(stopCode: string) {
-         const requestPromise = new Promise((resolve, reject) => {
+        const busPromise = this.getBusRequest(stopCode);
+
+        busPromise
+            .then((body)=>this.printBuses(body))
+            .catch(console.log);
+        
+    }
+
+    private getBusRequest(stopCode: string): Promise<Bus[]> {
+         const requestPromise = new Promise<Bus[]> ((resolve, reject) => {
             request('https://api.tfl.gov.uk/StopPoint/'+stopCode+'/Arrivals?app_id=1aeb84cc&app_key=8319a3e05b400574fdc7d0db5b0d3bfb', function (error, response, body) {
                 if (error) {
                     reject(error);
@@ -27,10 +106,10 @@ export class Template {
             });
             
         });
-        requestPromise.then((body)=>this.quickestFive(this.parseJson(body))).catch((err)=> console.log(err));
+        return requestPromise;
     }
 
-    private parseJson(body): Bus[] {
+    private parseJsonBus(body): Bus[] {
         const data = JSON.parse(body);
         let busList: Bus[] = [];
         //console.log(body);
@@ -44,7 +123,7 @@ export class Template {
         return busList;
     }
 
-    private quickestFive(busList: Bus[]): void {
+    private quickestFive(busList: Bus[]): Bus[] {
         let quickest: Bus[] = [];
         for (let i = 0; i < busList.length; i++) {
             if (quickest.length === 0) {
@@ -61,9 +140,7 @@ export class Template {
                 }
             }
         }
-        for (let i = 0; i< quickest.length; i++) {
-            console.log(quickest[i].getBusInfo());
-        }
+        return quickest;
     }
 }
 
